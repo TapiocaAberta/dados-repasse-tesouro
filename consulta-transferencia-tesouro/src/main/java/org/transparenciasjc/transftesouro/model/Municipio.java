d8d4718eb351dddc481f2b2eba39e27cae131140
William Antonio <william.fatecsjc@gmail.com>
2016-03-26 23:33:03 -0300

    Adicionando classes básicas

---
+++ consulta-transferencia-tesouro/src/main/java/org/transparenciasjc/transftesouro/model/Municipio.java
@@ -0,0 +1,74 @@
+package org.transparenciasjc.transftesouro.model;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@XmlRootElement
+@Entity
+@Table(name = "municipio")
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY, region = "cache-classes-basicas")
+public class Municipio {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "mun_id")
+	private long id;
+
+	@Column(name = "mun_nome")
+	private String nome;
+
+	@ManyToOne
+	@JoinColumn(name = "estado_est_id")
+	private Estado estado;
+
+	public Municipio() {
+		super();
+	}
+
+	public Municipio(String nome, Estado estado) {
+		super();
+		this.nome = nome;
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return nome + " - " + estado.getSigla();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+}

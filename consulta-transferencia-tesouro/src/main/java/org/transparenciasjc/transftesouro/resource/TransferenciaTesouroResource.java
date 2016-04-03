cf8c39c761bd52db132a1a919cbcd9d570093558
William Antonio <william.fatecsjc@gmail.com>
2016-04-03 05:48:14 -0300

    import nao usado

--- consulta-transferencia-tesouro/src/main/java/org/transparenciasjc/transftesouro/resource/TransferenciaTesouroResource.java
+++ consulta-transferencia-tesouro/src/main/java/org/transparenciasjc/transftesouro/resource/TransferenciaTesouroResource.java
@@ -11,7 +11,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 
 import org.transparenciasjc.transftesouro.controller.TransferenciaController;
 import org.transparenciasjc.transftesouro.model.Municipio;
